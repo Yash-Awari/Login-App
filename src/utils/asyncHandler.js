5ff335b7ac84c7d47d0f29e13104122b41c0c671
Yash-Awari <yashawari456@gmail.com>
2026-01-06 13:39:32 +0530

    initialize project structure with essential files and utility classes

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const asyncHandler= (requsetHandler)=>{
+    return(req,res,next)=>{
+        Promise.resolve(requsetHandler(req,res,next))
+        .catch((err) => next(err)) 
+    }
+}
+export {asyncHandler}
